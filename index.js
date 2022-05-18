993530fd63da52d333a17f08e7d097f86634cc25
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-19 00:41:28 +0600

    task create and get done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -8,8 +9,38 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dolir.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("userInformation").collection("user");
+
+        // task create
+        app.post('/user', async (req, res) => {
+            const newTask = req.body;
+            const result = await userCollection.insertOne(newTask);
+            res.send(result);
+        });
+
+        // Get all task
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const allTask = await cursor.toArray();
+            res.send(allTask);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
-    res.send('todo app using rest api');
+    res.send('Create task using rest api');
 })
 
 app.listen(port, () => {
